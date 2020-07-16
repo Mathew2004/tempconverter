ff332ca149791e660a87c348431f064f6d23ed91
Evan purification <61775436+financefamily@users.noreply.github.com>
2020-07-16 22:59:16 +0600

    temp converter

---
+++ script.js
@@ -0,0 +1,8 @@
+const cel = document.querySelector('#celcius > input');
+const fer = document.querySelector('#fer > input');
+const kel = document.querySelector('#kelvin > input');
+console.log(cel)
+cel.addEventListener('input',function(){
+	//const cTemp = cel.value;
+	console.log('cTemp')
+})
